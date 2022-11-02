f918f62617e97e0c4b604ff33ca5ca292c90db13
Countedragon <juanmontoya940@gmail.com>
2022-11-02 11:30:48 -0500

    Working animation for both dinos

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ clock = pygame.time.Clock()
 basic_font = pygame.font.Font(None, 50)
 fps_text = basic_font.render(str(fps), False, "Black")
 
-
 # Get images
 sky = pygame.image.load(
     "Free Platform Game Assets/Platform Game Assets/Background/png/1920x1080/Background/Background.png").convert()
@@ -41,6 +40,37 @@ mort_gravity = 0
 mort_direction = "RIGHT"
 
 
+# Animations
+class mort_animations:
+    all = mort_sheet.animation_list(24)
+    all_left = mort_sheet.animation_list(24, "LEFT")
+    idle = all[0:4]
+    run = all[5:11]
+    idle_left = all_left[0:4]
+    run_left = all_left[5:11]
+    current_animation = idle
+    frame = 0
+    current_time = 0
+    last_update = 0
+    cooldown = 200
+    last_Key = "RIGHT"
+
+
+class doux_animations:
+    all = doux_sheet.animation_list(24)
+    all_left = doux_sheet.animation_list(24, "LEFT")
+    idle = all[0:4]
+    run = all[5:11]
+    idle_left = all_left[0:4]
+    run_left = all_left[5:11]
+    current_animation = idle
+    frame = 0
+    current_time = 0
+    last_update = 0
+    cooldown = 200
+    last_Key = "RIGHT"
+
+
 # Player rectangles
 doux_rect0 = doux_frame_0.get_rect(bottomright=(50, 370))
 mort_rect0 = mort_frame_0.get_rect(bottomleft=(750, 370))
@@ -51,6 +81,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYUP:
+            mort_animations.frame = 0
+            doux_animations.frame = 0
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 if doux_rect0.y == 295:
@@ -62,17 +95,32 @@ while True:
     # Movement
     key = pygame.key.get_pressed()
     if key[K_d]:
-        doux_rect0.x += doux_vel
-        doux_direction = "RIGHT"
+        doux_rect0.x += mort_vel
+        doux_animations.current_animation = doux_animations.run
+        doux_animations.last_Key = "RIGHT"
     elif key[K_a]:
-        doux_rect0.x -= doux_vel
-        doux_direction = "LEFT"
+        doux_rect0.x -= mort_vel
+        doux_animations.current_animation = doux_animations.run_left
+        doux_animations.last_Key = "LEFT"
+    else:
+        if doux_animations.last_Key == "RIGHT":
+            doux_animations.current_animation = doux_animations.idle
+        else:
+            doux_animations.current_animation = doux_animations.idle_left
+
     if key[K_RIGHT]:
         mort_rect0.x += mort_vel
-        mort_direction = "RIGHT"
+        mort_animations.current_animation = mort_animations.run
+        mort_animations.last_Key = "RIGHT"
     elif key[K_LEFT]:
         mort_rect0.x -= mort_vel
-        mort_direction = "LEFT"
+        mort_animations.current_animation = mort_animations.run_left
+        mort_animations.last_Key = "LEFT"
+    else:
+        if mort_animations.last_Key == "RIGHT":
+            mort_animations.current_animation = mort_animations.idle
+        else:
+            mort_animations.current_animation = mort_animations.idle_left
 
     # Gravity
     doux_gravity += 1
@@ -90,14 +138,24 @@ while True:
         screen.blit(grass, (0 + i * vars.Grass.width, 350))
 
     # Players
-    if doux_direction == "RIGHT":
-        screen.blit(doux_frame_0, doux_rect0)
-    else:
-        screen.blit(doux_frame_0_left, doux_rect0)
-    if mort_direction == "RIGHT":
-        screen.blit(mort_frame_0, mort_rect0)
-    else:
-        screen.blit(mort_frame_0_left, mort_rect0)
+    mort_animations.current_time = pygame.time.get_ticks()
+    if mort_animations.current_time - mort_animations.last_update >= mort_animations.cooldown:
+        if mort_animations.frame + 1 < len(mort_animations.current_animation):
+            mort_animations.frame += 1
+        else:
+            mort_animations.frame = 0
+        mort_animations.last_update = mort_animations.current_time
+
+    doux_animations.current_time = pygame.time.get_ticks()
+    if doux_animations.current_time - doux_animations.last_update >= doux_animations.cooldown:
+        if doux_animations.frame + 1 < len(doux_animations.current_animation):
+            doux_animations.frame += 1
+        else:
+            doux_animations.frame = 0
+        doux_animations.last_update = doux_animations.current_time
+
+    screen.blit(mort_animations.current_animation[mort_animations.frame], mort_rect0)
+    screen.blit(doux_animations.current_animation[doux_animations.frame], doux_rect0)
 
     # Collisions
     if doux_rect0.colliderect(mort_rect0):
@@ -107,5 +165,5 @@ while True:
     fps_text = basic_font.render(str(fps), True, "Black")
     screen.blit(fps_text, (750, 0))
     pygame.display.update()
-    clock.tick()
+    clock.tick(60)
     fps = int(clock.get_fps())
--- methods.py
+++ methods.py
@@ -12,3 +12,20 @@ class SpriteSheet:
         image = pygame.transform.scale(image, (width * scale, height * scale))
         image.set_colorkey(color)
         return image
+
+    def get_image_left(self, frame, width, height, scale, color):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
+        image = pygame.transform.scale(image, (width * scale, height * scale))
+        image = pygame.transform.flip(image, True, False)
+        image.set_colorkey(color)
+        return image
+
+    def animation_list(self, num_frames, side="RIGHT"):
+        temp_list = []
+        for i in range(num_frames):
+            if side == "RIGHT":
+                temp_list.append(self.get_image(i, 24, 24, 3, (0, 0, 0)))
+            else:
+                temp_list.append(self.get_image_left(i, 24, 24, 3, (0, 0, 0)))
+        return temp_list
